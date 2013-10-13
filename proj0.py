dd9b9043507c228910b23f4a7c28e5be561c7b95
gchapa22 <gerardo.chapa.q@gmail.com>
2013-10-13 13:22:03 -0500

    Create proj0.py
    Mini-project description — “We want... a shrubbery!”
    
    This mini-project is optional. Specifically, your grade on this mini-project will not count towards your final grade in this class. While this mini-project may appear silly, its purpose is important: learning how to submit and peer assess your Python mini-projects via Coursera. This mini-project is your chance to run through this process and learn how to avoid some of the simple mistakes that have plagued students in previous sessions of this class. We will walk you through each step of the process so that, next week when it is time for Rock-Paper-Scissors-Lizard-Spock, you will understand the process completely.

---
+++ proj0.py
@@ -0,0 +1,5 @@
+# Your task is simple: modify this program template to print:
+# We want... a shrubbery!
+# in the CodeSkulptor console. Your program should consist of a single line of Python. 
+
+print 'We want... a shrubbery!'
